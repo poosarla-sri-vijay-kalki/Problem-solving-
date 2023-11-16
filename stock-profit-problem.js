183f8a0e539e9310b70010e07657ab5b0633b9f6
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-11-15 19:29:35 -0500

    update stock profit problem

---
+++ stock-profit-problem.js
@@ -0,0 +1,17 @@
+var a = [7, 12, 2, 13, 40, 5];
+
+const maxProfitFinder = (prices) => {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+
+  return maxProfit;
+};
+console.log(maxProfitFinder(a));
