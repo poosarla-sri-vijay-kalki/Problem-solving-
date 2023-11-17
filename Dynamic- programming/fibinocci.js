29ab7f150ebbbb8557ccec6e2ab1aea145103fbb
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-11-17 15:12:02 -0500

    fibonacci using memorization

--- Dynamic- programming/fibinocci.js
+++ Dynamic- programming/fibinocci.js
@@ -1,4 +1,5 @@
 var recursiveFibonacciIteratorCount = 0;
+var memorizationFibonacciIteratorCount = 0;
 
 /**
  *
@@ -14,6 +15,28 @@ function recursiveFibonacci(n) {
   return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
 }
 
+/**
+ *
+ * This function used to get the fibonacci series using a memorization
+ * The Time complexity is O(n)
+ * The Space complexity is O(n) +O(n)(for recursion stack + for memory)
+ * Time wise this is good but it takes more space.
+ *
+ */
+var fibonociiMemory = [];
+function memorizationFibonacci(n) {
+  memorizationFibonacciIteratorCount++;
+  if (n < fibonociiMemory.length && !isNaN(fibonociiMemory[n]))
+    return fibonociiMemory[n];
+  if (n <= 1) {
+    fibonociiMemory[n] = n;
+  } else {
+    fibonociiMemory[n] =
+      memorizationFibonacci(n - 1) + memorizationFibonacci(n - 2);
+  }
+  return fibonociiMemory[n];
+}
+
 // testing
 var testNumber = 10;
 console.log(recursiveFibonacci(testNumber));
@@ -21,4 +44,11 @@ console.log(
   `recursiveFibonacciIteratorCount for exectung for n = ${testNumber} is  ${recursiveFibonacciIteratorCount}`
 );
 recursiveFibonacciIteratorCount = 0;
+console.log("----------------------------------------------------");
+
+console.log(memorizationFibonacci(testNumber));
+console.log(
+  `memorizationFibonacciIteratorCount for exectung for n = ${testNumber} is  ${memorizationFibonacciIteratorCount}`
+);
+memorizationFibonacciIteratorCount = 0;
 console.log("----------------------------------------------------");
